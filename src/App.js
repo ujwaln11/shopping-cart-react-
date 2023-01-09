43bbbc8e1a6e6eb1c396abe2975c6d33393f88e4
Ujwal N <ujwaln18@gmail.com>
2023-01-09 18:11:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import Navbar from "./Components/Navbar";
+import ProductList from "./Components/ProductList";
+import Footer from "./Components/Footer";
+import React, { useState } from "react";
+import AddItem from "./Components/Additem";
+function App() {
+  const products = [
+    {
+      price: 99999,
+      name: "IPhone 14 Pro Max",
+      quantity: 0,
+    },
+    {
+      price: 9999,
+      name: "Redmi 14 Pro Max",
+      quantity: 0,
+    },
+  ];
+
+  let [productList, setProductList] = useState(products);
+  let [totalAmount, setTotalAmount] = useState(0);
+
+  const incrementQuantity = (index) => {
+    let newProductList = [...productList];
+    let newTotalAmount = totalAmount;
+    newProductList[index].quantity++;
+    newTotalAmount += newProductList[index].price;
+    setTotalAmount(newTotalAmount);
+    setProductList(newProductList);
+  };
+
+  const decrementQuantity = (index) => {
+    let newProductList = [...productList];
+    let newTotalAmount = totalAmount;
+    if (newProductList[index].quantity > 0) {
+      newProductList[index].quantity--;
+      newTotalAmount -= newProductList[index].price;
+    }
+    setTotalAmount(newTotalAmount);
+    setProductList(newProductList);
+  };
+
+  const resetQuantity = () => {
+    let newProductList = [...productList];
+    newProductList.map((products) => {
+      products.quantity = 0;
+    });
+    setProductList(newProductList);
+    setTotalAmount(0);
+  };
+
+  const removeItem = (index) => {
+    let newProductList = [...productList];
+    let newTotalAmount = totalAmount;
+    newTotalAmount-=newProductList[index].quantity * newProductList[index].price;
+    newProductList.splice(index, 1);
+    setProductList(newProductList);
+    setTotalAmount(newTotalAmount);
+  };
+
+  const addItem = (name,price) => {
+    let newProductList = [...productList];
+    newProductList.push({
+      price:price,
+      name:name,
+      quantity:0
+    })
+    setProductList(newProductList);
+  }
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <main className="container mt-5">
+        <AddItem addItem={addItem}/>
+        <ProductList
+          productList={productList}
+          incrementQuantity={incrementQuantity}
+          decrementQuantity={decrementQuantity}
+          removeItem={removeItem}
+        />
+      </main>
+      <Footer totalAmount={totalAmount} resetQuantity={resetQuantity} />
+    </>
+  );
+}
+export default App;
---
+++ src/Components/ProductList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Product from "./Product";
+
+export default function ProductList(props) {
+  return (
+    props.productList.length > 0 ? (
+    props.productList.map((product, i) => {
+    return (
+      <Product
+        product={product}
+        key={i}
+        incrementQuantity={props.incrementQuantity}
+        index={i}
+        decrementQuantity={props.decrementQuantity} 
+        removeItem={props.removeItem}
+      />
+    );
+  })
+  ) : (
+    <h1>No Products Exists In The Cart</h1>
+  )
+  )
+}
